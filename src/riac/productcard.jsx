1187a8657401c8a57d72dde569c5815ec8bbf733
Andrew Cho <andrewgunncho@gmail.com>
2021-09-10 14:08:09 -0700

    Creating a base

---
+++ src/riac/productcard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class ProductCard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // state
+
+    // binds
+  }
+
+  // functions
+
+  // component
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
