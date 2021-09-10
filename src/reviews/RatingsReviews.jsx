fb44d4cc28efad412c9c86bec6a2e88c4565102c
Daniel Prejs <danielnprejs@gmail.com>
2021-09-10 14:45:14 -0700

    Reviews component shell created

---
+++ src/reviews/RatingsReviews.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class RatingsReviews extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <h1>Hello R and R</h1>
+    )
+  }
+}
+
+export default RatingsReviews;
