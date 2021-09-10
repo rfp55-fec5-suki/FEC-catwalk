976d88ffee6a431658c7944bbbba28a42e364870
Andrew Cho <andrewgunncho@gmail.com>
2021-09-10 11:53:11 -0700

    Implemented ESLint with Hack Reactor Styling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+import './App.css';
 
-import React, { Component} from "react";
-import {hot} from "react-hot-loader";
-import "./App.css";
-
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
         <h1> Hello, React! </h1>
       </div>
     );
   }
 }
 
-export default App; 
+
+
+export default App;
