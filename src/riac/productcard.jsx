a755edcd2cd8c560f0df4105e6087afdaff1d4c3
Andrew Cho <andrewgunncho@gmail.com>
2021-09-12 14:00:22 -0700

    Added a sample product card with no info

--- src/riac/productcard.jsx
+++ src/riac/productcard.jsx
@@ -15,10 +15,32 @@ class ProductCard extends React.Component {
 
   render() {
     return (
-      <div></div>
+      <div className='riac-productcard-header'>
+
+        RELATED PRODUCTS
+
+        <div className='riac-productcard'>
+
+          {/* <a href={props.repo.html_url}>link: {props.repo.html_url} </a> */}
+
+          {/* <image> */}
+
+          <div> Product Category </div>
+
+          <div> Product Name </div>
+
+          <div> Price </div>
+
+          <div> Star Rating</div>
+
+        </div>
+
+      </div>
     );
   }
 }
 
-// module.exports = ProductCard
+export default ProductCard;
+
+// module.exports = ProductCard;
 // for testing
--- src/riac/productcard.test.jsx
+++ src/riac/productcard.test.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Button from './Button';
+// import React from 'react';
+// import { render, mount, shallow } from 'enzyme';
+// import ProductCard from './productcard.jsx';
 
-describe('Test Button component', () => {
-  it('Test click event', () => {
-    const mockCallBack = jest.fn();
+// const Enzyme = require('enzyme');
 
-    const button = shallow((<Button onClick={mockCallBack}>Ok!</Button>));
-    button.find('button').simulate('click');
-    expect(mockCallBack.mock.calls.length).toEqual(1);
-  });
-});
+// const EnzymeAdapter = require('enzyme-adapter-react-16');
+
+// Setup enzyme's react adapter
+// Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+// import Button from './Button';
+
+// describe('Test Button component', () => {
+//   it('Test click event', () => {
+//     const mockCallBack = jest.fn();
+
+//     const button = shallow((<Button onClick={mockCallBack}>Ok!</Button>));
+//     button.find('button').simulate('click');
+//     expect(mockCallBack.mock.calls.length).toEqual(1);
+//   });
+// });
+
+// const Fixture = function Fixture() {
+//   return (
+//     <div>
+//       <span className="foo" />
+//       <span className="bar baz" />
+//     </div>
+//   );
+// };
+// test('asdf', () => {
+//   const wrapper = mount(<Fixture />); // mount/render/shallow when applicable
+
+//   expect(wrapper.find('span')).toExist();
+//   expect(wrapper.find('ul')).not.toExist();
+// });
