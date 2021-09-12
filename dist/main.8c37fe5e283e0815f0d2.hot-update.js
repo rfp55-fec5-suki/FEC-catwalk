b51c9d84486f82c8c0d0b2bf3612bb34fa87471c
Daniel Prejs <danielnprejs@gmail.com>
2021-09-12 14:46:13 -0700

    added dependeny for svgs added svgs for stars

---
+++ dist/main.8c37fe5e283e0815f0d2.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate("main",{
+
+/***/ "./src/reviews/reviewTile/Stars/fullStar.svg":
+/*!***************************************************!*\
+  !*** ./src/reviews/reviewTile/Stars/fullStar.svg ***!
+  \***************************************************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/file-loader/dist/cjs.js):\\nError: ENOENT: no such file or directory, open '/home/dprejs/Desktop/hackreactor work/FEC/src/reviews/reviewTile/Stars/fullStar.svg'\");\n\n//# sourceURL=webpack:///./src/reviews/reviewTile/Stars/fullStar.svg?");
+
+/***/ })
+
+})
--- src/reviews/reviewTile/ReviewTile.jsx
+++ src/reviews/reviewTile/ReviewTile.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
-
+import emptyStar from './Stars/scoreComponents/emptyStar.svg';
+import quarterStar from './Stars/scoreComponents/25Star.svg';
+import halfStar from './Stars/scoreComponents/50Star.svg';
+import threeQStar from './Stars/scoreComponents/75Star.svg';
+import fullStar from './Stars/scoreComponents/fullStar.svg';
 
 class ReviewTile extends React.Component {
   constructor(props) {
@@ -10,6 +14,11 @@ class ReviewTile extends React.Component {
       <div>
         <div>
           Star Rating
+          <img src={fullStar} ></img>
+          <img src={threeQStar} ></img>
+          <img src={halfStar} ></img>
+          <img src={quarterStar} ></img>
+          <img src={emptyStar} ></img>
         </div>
         <div>
           Date of review
