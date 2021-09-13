6d643d5cf6d37745d3883b2675c3af5ec23f4f47
Qinyu Zhu <zz635186056@gmail.com>
2021-09-13 15:49:30 -0700

    PR

---
+++ dist/main.47768047d642e26d11ee.hot-update.js
@@ -0,0 +1,170 @@
+webpackHotUpdate("main",{
+
+/***/ "./config.js":
+false,
+
+/***/ "./node_modules/axios/index.js":
+false,
+
+/***/ "./node_modules/axios/lib/adapters/xhr.js":
+false,
+
+/***/ "./node_modules/axios/lib/axios.js":
+false,
+
+/***/ "./node_modules/axios/lib/cancel/Cancel.js":
+false,
+
+/***/ "./node_modules/axios/lib/cancel/CancelToken.js":
+false,
+
+/***/ "./node_modules/axios/lib/cancel/isCancel.js":
+false,
+
+/***/ "./node_modules/axios/lib/core/Axios.js":
+false,
+
+/***/ "./node_modules/axios/lib/core/InterceptorManager.js":
+false,
+
+/***/ "./node_modules/axios/lib/core/buildFullPath.js":
+false,
+
+/***/ "./node_modules/axios/lib/core/createError.js":
+false,
+
+/***/ "./node_modules/axios/lib/core/dispatchRequest.js":
+false,
+
+/***/ "./node_modules/axios/lib/core/enhanceError.js":
+false,
+
+/***/ "./node_modules/axios/lib/core/mergeConfig.js":
+false,
+
+/***/ "./node_modules/axios/lib/core/settle.js":
+false,
+
+/***/ "./node_modules/axios/lib/core/transformData.js":
+false,
+
+/***/ "./node_modules/axios/lib/defaults.js":
+false,
+
+/***/ "./node_modules/axios/lib/helpers/bind.js":
+false,
+
+/***/ "./node_modules/axios/lib/helpers/buildURL.js":
+false,
+
+/***/ "./node_modules/axios/lib/helpers/combineURLs.js":
+false,
+
+/***/ "./node_modules/axios/lib/helpers/cookies.js":
+false,
+
+/***/ "./node_modules/axios/lib/helpers/isAbsoluteURL.js":
+false,
+
+/***/ "./node_modules/axios/lib/helpers/isAxiosError.js":
+false,
+
+/***/ "./node_modules/axios/lib/helpers/isURLSameOrigin.js":
+false,
+
+/***/ "./node_modules/axios/lib/helpers/normalizeHeaderName.js":
+false,
+
+/***/ "./node_modules/axios/lib/helpers/parseHeaders.js":
+false,
+
+/***/ "./node_modules/axios/lib/helpers/spread.js":
+false,
+
+/***/ "./node_modules/axios/lib/helpers/validator.js":
+false,
+
+/***/ "./node_modules/axios/lib/utils.js":
+false,
+
+/***/ "./node_modules/axios/package.json":
+false,
+
+/***/ "./node_modules/css-loader/index.js!./src/App.css":
+false,
+
+/***/ "./node_modules/css-loader/lib/css-base.js":
+false,
+
+/***/ "./node_modules/fast-levenshtein/levenshtein.js":
+false,
+
+/***/ "./node_modules/hoist-non-react-statics/dist/hoist-non-react-statics.cjs.js":
+false,
+
+/***/ "./node_modules/process/browser.js":
+false,
+
+/***/ "./node_modules/prop-types/factoryWithTypeCheckers.js":
+false,
+
+/***/ "./node_modules/prop-types/index.js":
+false,
+
+/***/ "./node_modules/react-hot-loader/dist/react-hot-loader.development.js":
+false,
+
+/***/ "./node_modules/react-hot-loader/dist/react-hot-loader.production.min.js":
+false,
+
+/***/ "./node_modules/react-hot-loader/index.js":
+false,
+
+/***/ "./node_modules/react-is/cjs/react-is.development.js":
+false,
+
+/***/ "./node_modules/react-is/index.js":
+false,
+
+/***/ "./node_modules/react-lifecycles-compat/react-lifecycles-compat.es.js":
+false,
+
+/***/ "./node_modules/shallowequal/index.js":
+false,
+
+/***/ "./node_modules/style-loader/lib/addStyles.js":
+false,
+
+/***/ "./node_modules/style-loader/lib/urls.js":
+false,
+
+/***/ "./node_modules/webpack/buildin/amd-define.js":
+false,
+
+/***/ "./node_modules/webpack/buildin/amd-options.js":
+false,
+
+/***/ "./node_modules/webpack/buildin/harmony-module.js":
+false,
+
+/***/ "./node_modules/webpack/buildin/module.js":
+false,
+
+/***/ "./src/App.css":
+false,
+
+/***/ "./src/App.js":
+/*!********************!*\
+  !*** ./src/App.js ***!
+  \********************/
+/*! exports provided: default */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: /Users/zhuqinyu/HR/FEC/FEC-catwalk/src/App.js: Unexpected token (4:1)\\n\\n\\u001b[0m \\u001b[90m 2 |\\u001b[39m \\u001b[90m// import { hot } from 'react-hot-loader';\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 3 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[32m'./App.css'\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 4 |\\u001b[39m \\u001b[33m<<\\u001b[39m\\u001b[33m<<\\u001b[39m\\u001b[33m<<\\u001b[39m\\u001b[33m<\\u001b[39m \\u001b[33mHEAD\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m   |\\u001b[39m  \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 5 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[33mQAList\\u001b[39m \\u001b[36mfrom\\u001b[39m \\u001b[32m'./Q&A/QAList.jsx'\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 6 |\\u001b[39m \\u001b[33m===\\u001b[39m\\u001b[33m===\\u001b[39m\\u001b[33m=\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 7 |\\u001b[39m \\u001b[36mimport\\u001b[39m \\u001b[33mReviews\\u001b[39m \\u001b[36mfrom\\u001b[39m \\u001b[32m'./reviews/RatingsReviews.jsx'\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n    at Object._raise (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:807:17)\\n    at Object.raiseWithData (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:800:17)\\n    at Object.raise (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:761:17)\\n    at Object.unexpected (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:3266:16)\\n    at Object.jsxParseIdentifier (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:6942:12)\\n    at Object.jsxParseNamespacedName (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:6952:23)\\n    at Object.jsxParseElementName (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:6963:21)\\n    at Object.jsxParseOpeningElementAt (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:7050:22)\\n    at Object.jsxParseElementAt (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:7083:33)\\n    at Object.jsxParseElement (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:7157:17)\\n    at Object.parseExprAtom (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:7164:19)\\n    at Object.parseExprSubscripts (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:11119:23)\\n    at Object.parseUpdate (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:11099:21)\\n    at Object.parseMaybeUnary (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:11077:23)\\n    at Object.parseMaybeUnaryOrPrivate (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:10900:77)\\n    at Object.parseExprOps (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:10907:23)\\n    at Object.parseMaybeConditional (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:10877:23)\\n    at Object.parseMaybeAssign (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:10835:21)\\n    at Object.parseExpressionBase (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:10775:23)\\n    at /Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:10769:39\\n    at Object.allowInAnd (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:12619:16)\\n    at Object.parseExpression (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:10769:17)\\n    at Object.parseStatementContent (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:12957:23)\\n    at Object.parseStatement (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:12826:17)\\n    at Object.parseBlockOrModuleBlockBody (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:13415:25)\\n    at Object.parseBlockBody (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:13406:10)\\n    at Object.parseProgram (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:12748:10)\\n    at Object.parseTopLevel (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:12739:25)\\n    at Object.parse (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:14479:10)\\n    at parse (/Users/zhuqinyu/HR/FEC/FEC-catwalk/node_modules/@babel/parser/lib/index.js:14531:38)\");\n\n//# sourceURL=webpack:///./src/App.js?");
+
+/***/ }),
+
+/***/ "./src/Q&A/QAList.jsx":
+false
+
+})
