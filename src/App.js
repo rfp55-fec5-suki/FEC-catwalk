7c0a7b27932e897cc0b92790017fff42df6ce407
Qinyu Zhu <zz635186056@gmail.com>
2021-09-13 10:29:36 -0700

    component framework

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
 import './App.css';
+import QList from './Q&A/Qlist.jsx'
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <h1> Hello, React! </h1>
+        <QList />
       </div>
     );
   }
---
+++ src/Q&A/QAList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class QList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return(
+      <div>
+        Question Lists
+      </div>
+    )
+  }
+}
+
+export default QList;
