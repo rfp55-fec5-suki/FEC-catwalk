e2716daea97a1c6a1f57d35bb74dc0d02f2858de
Andrew Cho <andrewgunncho@gmail.com>
2021-09-13 11:28:02 -0700

    Merged with master for new access to token

--- src/riac/productcard.jsx
+++ src/riac/productcard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import token from './config.js';
+import token from '../../config.js';
 
 class ProductCard extends React.Component {
   constructor(props) {
