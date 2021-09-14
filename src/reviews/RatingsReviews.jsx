15cc7d7eaab67137ca4918660f4165cb31ed4e93
Daniel Prejs <danielnprejs@gmail.com>
2021-09-13 18:42:44 -0700

    Ratings and Reviews refactored to use hooks

--- src/reviews/RatingsReviews.jsx
+++ src/reviews/RatingsReviews.jsx
@@ -1,21 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReviewList from './ReviewList.jsx'
 import mockData from './mockData.js';
 import './RatingsReviews.css';
 var reviews = mockData.getReviews.results;
 
-class RatingsReviews extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return(
-      <div className="rr-main">
-        RATINGS AND REVIEWS
-        <ReviewList reviews={reviews}/>
-      </div>
-    )
-  }
+const RatingsReviews = (props) => {
+
+
+  return (
+    <div className="rr-main">
+      RATINGS AND REVIEWS
+      <ReviewList reviews={reviews} />
+    </div>
+  )
 }
 
 export default RatingsReviews;
