18a7a1307213e62ed617c1cdc352afc54b79a90a
Qinyu Zhu <zz635186056@gmail.com>
2021-09-14 11:16:12 -0700

    change some test

--- src/Q&A/Q&A.test.jsx
+++ src/Q&A/Q&A.test.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import '@testing-library/jest-dom';
-import * from 'enzyme';
-import * as Adapter from 'enzyme-adapter-react-16';
+import { render, screen } from '@testing-library/react';
 
-enzyme.configure
+import QAList from './QAList.jsx';
+
+describe('QAList', () => {
+  test('renders App component', () => {
+    render(<QAList />);
+  });
+});
+
