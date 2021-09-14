9f96e5087c0139395b8eba90dae9b9da6cc27ab7
Nan Jiang <dora.nan.j@gmail.com>
2021-09-13 21:39:29 -0700

    pull master

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React, { Component } from 'react';
 // import { hot } from 'react-hot-loader';
 import './App.css';
-<<<<<<< HEAD
 import Overview from './overview/Overview.jsx';
-=======
 import QAList from './Q&A/QAList.jsx'
 import Reviews from './reviews/RatingsReviews.jsx';
 
 import ProductCard from './riac/productcard.jsx';
->>>>>>> master
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <h1> Hello, React! </h1>
-<<<<<<< HEAD
         <Overview />
-=======
         <ProductCard />
         <QAList />
         <Reviews />
->>>>>>> master
+
       </div>
 
     );
