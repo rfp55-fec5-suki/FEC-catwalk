1a33a8a9891b8854d11fe4dfb4aef5dfcb5a853e
Daniel Prejs <danielnprejs@gmail.com>
2021-09-13 17:34:36 -0700

    Review list renders the first 2 reviews from the list passed in props

--- src/reviews/ReviewList.jsx
+++ src/reviews/ReviewList.jsx
@@ -3,33 +3,15 @@ import React, { useState } from 'react';
 import ReviewTile from './reviewTile/ReviewTile.jsx';
 
 const ReviewList = (props) => {
-  // var reviewList = props.reviews.slice(0,2);
-  const [count, setCount] = useState(0);
-  // const [state, setState] = useState({renderList: props.reviews.slice(0,2)})
-  // const renderList = state.renderList;
+  const [state, setState] = useState({renderList: props.reviews.slice(0,2)})
+  const renderList = state.renderList;
   return (
   <div>
     hooks yay
-    {/* {renderList.map((review) => <ReviewTile review={review} />)} */}
-    {/* <ReviewTile review={reviewList[0]}/>
-    <ReviewTile review={reviewList[1]}/> */}
+    {renderList.map((review) => <ReviewTile review={review} />)}
   </div>
   );
 }
-
-// function Example() {
-//   // Declare a new state variable, which we'll call "count"
-//   const [count, setCount] = useState(0);
-
-//   return (
-//     <div>
-//       <p>You clicked {count} times</p>
-//       <button onClick={() => setCount(count + 1)}>
-//         Click me
-//       </button>
-//     </div>
-//   );
-// }export default Example;
 
 
 export default ReviewList;
