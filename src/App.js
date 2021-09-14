adc5226858b0e8d16092db828c2050178489dbc2
Daniel Prejs <danielnprejs@gmail.com>
2021-09-13 17:14:21 -0700

    updated react

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1> Hello, React! </h1>
+        <h1> Best E-Commerce App EVER! </h1>
         <ProductCard />
         <QAList />
         <Reviews />
--- src/reviews/ReviewList.jsx
+++ src/reviews/ReviewList.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import mockData from './mockData.js'
 import ReviewTile from './reviewTile/ReviewTile.jsx';
 
 const ReviewList = (props) => {
-  var reviewList = props.reviews;
+  // var reviewList = props.reviews.slice(0,2);
+  const [count, setCount] = useState(0);
+  // const [state, setState] = useState({renderList: props.reviews.slice(0,2)})
+  // const renderList = state.renderList;
   return (
   <div>
-    <ReviewTile review={reviewList[0]}/>
-    <ReviewTile review={reviewList[1]}/>
+    hooks yay
+    {/* {renderList.map((review) => <ReviewTile review={review} />)} */}
+    {/* <ReviewTile review={reviewList[0]}/>
+    <ReviewTile review={reviewList[1]}/> */}
   </div>
-  )
-};
+  );
+}
+
+// function Example() {
+//   // Declare a new state variable, which we'll call "count"
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <p>You clicked {count} times</p>
+//       <button onClick={() => setCount(count + 1)}>
+//         Click me
+//       </button>
+//     </div>
+//   );
+// }export default Example;
+
 
 export default ReviewList;
