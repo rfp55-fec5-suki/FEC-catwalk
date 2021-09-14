c67ab69a71bfa31a1f95fccd416daefa94fda0c4
Daniel Prejs <danielnprejs@gmail.com>
2021-09-14 13:36:44 -0700

    more reviews button now adds reviews 2 at a time

--- src/reviews/RatingsReviews.jsx
+++ src/reviews/RatingsReviews.jsx
@@ -4,25 +4,32 @@ import ReviewList from './ReviewList.jsx';
 import token from '../../config.js';
 import mockData from './mockData.js';
 import './RatingsReviews.css';
-var mockReviews = mockData.getReviews.results;
-var results = [];
+// var mockReviews = mockData.getReviews.results;
+// var results = [];
+var reviewPage = 1;
 const RatingsReviews = (props) => {
   const [reviews, setReviews] = useState(() => {
-    console.log('use state function run')
-    return mockReviews
+    // console.log('use state function run')
+    return []
   })
 
+  const moreReviews = () => {
+    // console.log(reviewPage)
+    reviewPage++;
+    getReviewList()
+  }
   const getReviewList = (product_id = 40344) => {
     // console.log(product_id)
+    // console.log(reviewPage)
     axios({
       method: 'get',
-      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews?product_id=${product_id}`,
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews?product_id=${product_id}&count=2&page=${reviewPage}`,
       headers: {
         'Authorization': token.TOKEN
       }
     }).then((response) => {
       // console.log(response.data.results)
-      setReviews(response.data.results)
+      setReviews([...reviews, ...response.data.results])
     }).catch((err) => {
       console.log('error getting review list from api', err)
     })
@@ -32,9 +39,9 @@ const RatingsReviews = (props) => {
   }, [props.product_id])
 
   return (
-    <div className="rr-main" onClick={() => getReviewList()} >
+    <div className="rr-main" >
       RATINGS AND REVIEWS
-      <ReviewList reviews={reviews} />
+      <ReviewList reviews={reviews} more={() => {moreReviews()}}/>
     </div>
   )
 }
