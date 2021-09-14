8922a8345831bac6df4680694f241faca29f398c
Qinyu Zhu <zz635186056@gmail.com>
2021-09-14 14:37:45 -0700

    .

--- src/Q&A/QAAnswer.jsx
+++ src/Q&A/QAAnswer.jsx
@@ -1,33 +1,11 @@
 import React from 'react';
 
-// import token from '../../config.js';
-// import axios from 'axios';
 
 class QAAnswer extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   answers: []
     }
-    // this.getAnswers = this.getAnswers.bind(this);
 
-  // componentDidMount() {
-  //   this.getAnswers()
-  //}
-
-  // getAnswers() {
-  //   const question_id = this.props.questions.results.question_id
-  //   axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/qa/questions/${question_id}/answers`, {headers: {'Authorization': token.TOKEN}})
-  //     .then((res) => {
-  //       console.log('react get answers success: ', res.data);
-  //       this.setState({
-  //         answers: res.data.results
-  //       });
-  //     })
-  //     .catch((error) => {
-  //       throw error;
-  //     })
-  // }
 
   render() {
     console.log('this.props: ', this.props)
