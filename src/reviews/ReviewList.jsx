901a6d269318a0de12d7385abec1f70a268d554f
Daniel Prejs <danielnprejs@gmail.com>
2021-09-14 17:07:31 -0700

    styling added to review list

--- src/reviews/ReviewList.jsx
+++ src/reviews/ReviewList.jsx
@@ -6,18 +6,22 @@ const ReviewList = (props) => {
   // const [nReviews, setNReviews] = useState(2)
 
   return (
-  <div>
-    <div>
+  <div className='review-list'>
+    <div className='sort'>
       Sort Reviews by
-    <select onChange={props.sort}>
+    {/* </div> */}
+    {/* <div className='sort-select'> */}
+    <select onChange={props.sort} >
       <option value='relevant'>Relevent</option>
       <option value='newest'>Newest</option>
       <option value='helpful'>Helpful</option>
     </select>
     </div>
+    <div className='rr-tile-list'>
     {reviews.map((review) => <ReviewTile review={review} key={review.review_id}/>)}
-    {props.renderButton ? <button className='more-reviews' onClick={props.more}>More Reviews</button> : null}
-    <button>Add Review +</button>
+    </div>
+    {props.renderButton ? <button className='rr-button more-reviews' onClick={props.more}>More Reviews</button> : null}
+    <button className='rr-button add-review'>Add Review +</button>
   </div>
   );
 }
