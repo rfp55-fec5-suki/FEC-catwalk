11adcb2d51db2f3eb039e5c160a44355d86c2d91
Daniel Prejs <danielnprejs@gmail.com>
2021-09-15 13:01:54 -0700

    review list contains all of the reviews when there are items in rating filter

--- src/reviews/RatingsReviews.jsx
+++ src/reviews/RatingsReviews.jsx
@@ -18,6 +18,7 @@ class RatingsReviews extends React.Component {
       filterByRating: []
     }
     this.getReviewList = this.getReviewList.bind(this);
+    this.getAllReviews = this.getAllReviews.bind(this);
     this.moreReviews = this.moreReviews.bind(this);
     this.sortChange = this.sortChange.bind(this);
     this.setRatingFilter = this.setRatingFilter.bind(this);
@@ -28,19 +29,26 @@ class RatingsReviews extends React.Component {
       var newfilter = this.state.filterByRating.filter((filterValue) => {
         return filterValue !== rating;
       })
-      this.setState({filterByRating: newfilter});
+      this.setState({filterByRating: newfilter}, () => {
+        if(this.state.filterByRating.length === 0) {
+          this.state.reviews = [];
+          this.getReviewList();
+        }
+      });
     } else {
-      this.setState({filterByRating: [...this.state.filterByRating, rating]})
+      this.setState({filterByRating: [...this.state.filterByRating, rating]}, () => {
+        this.getAllReviews();
+      });
     }
   }
   //Review list handlers --------------------------
   sortChange(e) {
     e.preventDefault();
     console.log(e.target.value);
-    sort = e.target.value
+    sort = e.target.value;
     reviewPage = 1;
     this.setState({reviews: []}, () => {
-      this.getReviewList()
+      this.getReviewList();
     });
   }
   //api call handlers -----------------------------
@@ -52,10 +60,24 @@ class RatingsReviews extends React.Component {
         'Authorization': token.TOKEN
       }
     }).then((response) => {
-      console.log(response.data)
-      this.setState({meta: response.data})
+      // console.log(response.data)
+      this.setState({meta: response.data});
     }).catch((err) => {
       console.log(err);
+    })
+  }
+  getAllReviews() {
+    axios({
+      method: 'get',
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews?product_id=${this.props.product_id}&count=100`,
+      headers: {
+        'Authorization': token.TOKEN
+      }
+    }).then((response) => {
+      console.log('got all reviews')
+      this.setState({reviews: response.data.results});
+    }).catch((err) => {
+      console.log('error getting all reviews to then filter by rating', err);
     })
   }
   getReviewList() {
@@ -66,7 +88,7 @@ class RatingsReviews extends React.Component {
         'Authorization': token.TOKEN
       }
     }).then((response) => {
-      this.setState({reviews: [...this.state.reviews, ...response.data.results]})
+      this.setState({reviews: [...this.state.reviews, ...response.data.results]});
       axios({
         method: 'get',
         url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews?product_id=${this.props.product_id}&count=2&page=${reviewPage + 1}`,
@@ -75,21 +97,22 @@ class RatingsReviews extends React.Component {
         }
       }).then((response => {
         if (response.data.results.length === 0) {
-          this.setState({ hasMoreReviews: false })
+          this.setState({ hasMoreReviews: false });
         }
       }))
     }).catch((err) => {
-      console.log('error getting review list from api', err)
+      console.log('error getting review list from api', err);
     })
   }
   moreReviews() {
     reviewPage++;
-    this.getReviewList()
+    this.getReviewList();
   }
   //lifecycle functions ---------------------------------------------
   componentDidUpdate(oldProps) {
     if(oldProps.product_id !== this.props.product_id) {
       this.state.reviews = [];
+      this.state.filterByRating = [];
       this.getReviewList();
       this.getMeta();
     }
@@ -104,7 +127,7 @@ class RatingsReviews extends React.Component {
         RATINGS & REVIEWS
         <RatingBreakdown meta={this.state.meta} filter={this.setRatingFilter}/>
         <ReviewList reviews={this.state.reviews} more={this.moreReviews} sort={this.sortChange}
-        renderButton={this.state.hasMoreReviews} meta={this.state.meta}/>
+        renderButton={this.state.hasMoreReviews} meta={this.state.meta} filter={this.state.filterByRating}/>
       </div>
     )
   }
