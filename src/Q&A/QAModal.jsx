71300ee3b81acdb6f2352141fc18e50a708449bd
Qinyu Zhu <zz635186056@gmail.com>
2021-09-15 18:44:52 -0700

    add modal content

--- src/Q&A/QAModal.jsx
+++ src/Q&A/QAModal.jsx
@@ -20,6 +20,12 @@ const Modal = (props) => {
     <div className={showHideClassName}>
       <section className='modal-main'>
         {children}
+        <div className='modal-header'>
+          <h4 className='modal-title'>Add your answer</h4>
+        </div>
+        <div className='modal-body'>
+          Answer form here
+        </div>
         <button type='submit' onClick={handleClose}>
           Submit
         </button>
