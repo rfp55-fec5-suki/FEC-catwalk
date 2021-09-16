9acd90dc5d78314d7aec243162f7a519bcf9fbf5
Qinyu Zhu <zz635186056@gmail.com>
2021-09-16 10:46:27 -0700

    add img for answers But looks ugly

--- src/Q&A/Q&AEachAnswer.jsx
+++ src/Q&A/Q&AEachAnswer.jsx
@@ -13,12 +13,15 @@ class EachAnswer extends React.Component {
 
   render() {
     const answer = this.props.answer;
+    const photos = this.props.answer.photos; //photos here are array, let's retrieve the url
+
     return (
       <div key = {answer.id}>
         <div>
           {answer.body}
         </div>
         <div>
+          {photos.map(photo => <img src = {`${photo}`}/>)}
           by {answer.answerer_name}, {answer.date.substring(0,10)}
         </div>
         <div>
