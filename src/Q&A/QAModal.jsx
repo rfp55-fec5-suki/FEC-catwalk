ac2d63508071c930e9eddfb76bb19234f19a6c5b
Qinyu Zhu <zz635186056@gmail.com>
2021-09-16 01:20:37 -0700

    edit the form

--- src/Q&A/QAModal.jsx
+++ src/Q&A/QAModal.jsx
@@ -12,13 +12,22 @@ class Modal extends React.Component {
       question: false,
       answer: false
     }
+
+    this.handleFormChange = this.handleFormChange.bind(this)
+    this.submitForm = this.submitForm.bind(this)
+  }
+
+  handleFormChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
   }
 
   //event handler here
   submitForm(e) {
     const { body, name, email, photo, answer } = this.state
     const { postAnswer, postQuestion, product } = this.props
-    const input = (answer ? { body, name, email, photo } : { body, name, email, product.id })
+    const input = (answer ? { body, name, email, photo } : { body, name, email, product })
 
     e.preventDefault()
 
@@ -32,27 +41,45 @@ class Modal extends React.Component {
       body: '',
       name: '',
       email: '',
-      photo: ''
+      photo: []
     })
 
-    this.toggleModal()
+    // this.toggleModal()
   }
 
+  // toggleModal
+
   render() {
+    const question = this.state.question
+    const product = this.props.product
+    const eachQuestion  = this.props.question
     const handleClose = this.props.handleClose;
     const show = this.props.show;
     const children = this.props.children;
     const showHideClassName = show ? 'modal display-block' : 'modal display-none';
+    const text = (question ? 'Question: ' : 'Answer: ')
+    const titleText = (question ? 'Ask your Question: ' : 'Submit your Answer')
+    const subtitleText = (question ? 'About the Product: ' : `${product.name}: ${eachQuestion.question_body}`)
 
     return (
       <div className={showHideClassName}>
         <section className='modal-main'>
           {children}
           <div className='modal-header'>
-            <h4 className='modal-title'>Add your answer</h4>
+            <h3>{titleText}</h3>
+            <p>
+              <strong>{subtitleText}</strong>
+            </p>
           </div>
           <div className='modal-body'>
-            Answer form here
+            <form onSubmit={(e) => {this.submitForm(e)}} name='QA'>
+              <div>
+                {/* <label></label>
+                <label></label>
+                <label></label> */}
+                See the form
+              </div>
+            </form>
           </div>
           <button type='submit' onClick={handleClose}>
             Submit
