a0f3cb0a6342a29fc8c84912f66ffe58198d3b79
Andrew Cho <andrewgunncho@gmail.com>
2021-09-17 11:37:01 -0700

    Switched addoutfit to class

--- src/riac/outfitlist/addoutfit.jsx
+++ src/riac/outfitlist/addoutfit.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const AddOutfit = (props) => (
-  <div>Hello World!</div>
-)
+class AddOutfit extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+
+  render() {
+    return(
+      <div>Hello World!</div>
+    )
+  }
+}
+
+
 
 export default AddOutfit;
