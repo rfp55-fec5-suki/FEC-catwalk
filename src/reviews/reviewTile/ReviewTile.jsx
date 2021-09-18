c585907bfa4201130e0ba7b0546a019e88926ea5
Daniel Prejs <danielnprejs@gmail.com>
2021-09-17 18:07:46 -0700

    img modal now works

--- src/reviews/reviewTile/ReviewTile.jsx
+++ src/reviews/reviewTile/ReviewTile.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import StarRating from './../../sharedComponents/StarRating.jsx';
 import checkmark from './Checkmark.svg';
+import Modal from '../Modal.jsx';
 import './reviewTile.css';
 
 class ReviewTile extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      fullImgUrl: '',
+      showFullImg: false
+    }
     this.monthNames = ["January", "February", "March", "April", "May", "June",
       "July", "August", "September", "October", "November", "December"];
     var date = new Date(props.review.date);
@@ -20,6 +25,14 @@ class ReviewTile extends React.Component {
       this.summary = props.review.summary;
       this.summaryCont = false
     }
+    this.showImgModal = this.showImgModal.bind(this);
+    this.hideImgModal = this.hideImgModal.bind(this);
+  }
+  showImgModal(url) {
+    this.setState({fullImgUrl: url, showFullImg: true});
+  }
+  hideImgModal() {
+    this.setState({showFullImg: false});
   }
   render() {
     return (
@@ -42,7 +55,8 @@ class ReviewTile extends React.Component {
             {this.props.review.body}
           </div>
           <div>
-            {this.props.review.photos.length ? this.props.review.photos.map((photo) => (<img src ={`${photo.url}`} className='tile-thumb' key={ photo.id } />)) : null}
+            {this.props.review.photos.length ? this.props.review.photos.map((photo) => (<img src ={`${photo.url}`}
+            className='tile-thumb' key={ photo.id }  onClick={() => this.showImgModal(photo.url)}/>)) : null}
           </div>
         </div>
         {this.props.review.recommend ? <div className='recommend' data-testid='reviewTileRecommend'>
@@ -54,6 +68,8 @@ class ReviewTile extends React.Component {
         <div className='helpful' data-testid='reviewTileHelpful'>
           Helpful? Yes({this.props.review.helpfulness}) | report
         </div>
+
+        <Modal show={this.state.showFullImg} children={<img src={this.state.fullImgUrl} />} handleClose={this.hideImgModal}/>
       </div>
     )
   }
