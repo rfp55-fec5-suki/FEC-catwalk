d525cd44b721a9d1936373d9c3b1eb33ab164dae
Daniel Prejs <danielnprejs@gmail.com>
2021-09-20 10:22:04 -0700

    Revert "Merge branch 'master' of https://github.com/rfp55-fec5-suki/FEC-catwalk"
    This reverts commit 0c3b16099c384020750a6181dc97101f96d0305d, reversing
    changes made to 1a81e461c0066d2bd730f57c4e116a4495fb3960.

--- src/Q&A/Q&AEachAnswer.jsx
+++ src/Q&A/Q&AEachAnswer.jsx
@@ -1,109 +1,37 @@
 import React from 'react';
-import axios from 'axios';
-import token from '../../config.js';
 
 
 class EachAnswer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      helpness: this.props.answer.helpfulness,
-      reported: false
+      helpness: this.props.answer.helpfulness
     }
-
-    this.addHelp = this.addHelp.bind(this);
-    this.handleClickYes = this.handleClickYes.bind(this);
-    this.markReport = this.markReport.bind(this);
-    this.handleClickReport = this.handleClickReport.bind(this);
   }
 
   //after onclick helpful?, this.setState : helpness+1
-  addHelp(num) {
-    const answer_id = this.props.answer.id;
-    axios({
-      method: 'put',
-      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/qa/answers/${answer_id}/helpful`,
-      params: answer_id,
-      data: num,
-      headers: {
-        'Authorization': token.TOKEN
-      }
-    })
-      .then((res) => {
-        this.props.getQuestions();
-      })
-      .catch((error) => {
-        throw error;
-      })
-  }
-
-  markReport(input) {
-    const answer_id = this.props.answer.id;
-    axios({
-      method: 'put',
-      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/qa/answers/${answer_id}/report`,
-      parames: answer_id,
-      data: input,
-      headers: {
-        'Authorization': token.TOKEN
-      }
-    })
-      .then((res) => {
-        this.props.getQuestions();
-      })
-      .catch((error) => {
-        throw error;
-      })
-  }
-
-  handleClickYes() {
-    const answer_id = this.props.answer.id;
-    this.setState(prevState => {
-      return {
-        helpfulness: prevState.helpfulness + 1
-      }
-    })
-    this.addHelp(answer_id);
-  }
-
-  handleClickReport() {
-    const answer_id = this.props.answer.id;
-    this.setState({
-      reported: true
-    })
-    this.markReport(answer_id, {});
-  }
 
   render() {
     const answer = this.props.answer;
     const photos = this.props.answer.photos; //photos here are array, let's retrieve the url
+
     return (
       <div key = {answer.id}>
         <div>
           {answer.body}
         </div>
         <div>
-          helpful count: {this.props.answer.helpfulness}
-        </div>
-        <div>
-          {photos.map(photo => <img key = {answer.id} src = {`${photo}`}/>)}
-          by
-          <div>
-            {(answer.answerer_name === 'Seller') && <b>{answer.answerer_name}</b>}
-            {(answer.answerer_name !== 'Seller') && <div>{answer.answerer_name}</div>}
-          </div>
-          <div>
-            {answer.date.substring(0,10)}
-          </div>
+          {photos.map(photo => <img src = {`${photo}`}/>)}
+          by {answer.answerer_name}, {answer.date.substring(0,10)}
         </div>
         <div>
           helpful?
-          <button type = 'submit' onClick={this.handleClickYes}>Yes({this.props.answer.helpfulness})</button>
-          <div>{this.props.answer.helpfulness}</div>
-          <button type = 'submit' onClick={this.handleClickReport}>Report</button>
+          <button type = 'submit'>Yes</button>
+          <button type = 'submit'>Report</button>
         </div>
       </div>
     )
+
   }
 }
 
